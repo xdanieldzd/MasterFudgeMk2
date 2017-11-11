xdanieldzd/MasterFudgeMk2

commit 4b3e2dc172f320a73b46ebb8aa8e6f2cb0d67450

More A/V backend stuff, UI changes, etc

MasterFudgeMk2/Machines/BaseMachine.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
-using MasterFudgeMk2.Common.AudioBackend;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 
 namespace MasterFudgeMk2.Machines
@@ -15,8 +12,11 @@ namespace MasterFudgeMk2.Machines
         public abstract string FriendlyName { get; }
         public abstract string FriendlyShortName { get; }
         public abstract string FileFilter { get; }
+
         public abstract double RefreshRate { get; }
         public abstract float AspectRatio { get; }
+        public abstract Rectangle ScreenViewport { get; }
+
         public abstract bool SupportsBootingWithoutMedia { get; }
         public abstract bool CanCurrentlyBootWithoutMedia { get; }
         public abstract MachineConfiguration Configuration { get; set; }
@@ -25,9 +25,6 @@ namespace MasterFudgeMk2.Machines
 
         public event EventHandler<RenderScreenEventArgs> RenderScreen;
         protected virtual void OnRenderScreen(RenderScreenEventArgs e) { RenderScreen?.Invoke(this, e); }
-
-        public event EventHandler<ScreenViewportChangeEventArgs> ScreenViewportChange;
-        protected virtual void OnScreenViewportChange(ScreenViewportChangeEventArgs e) { ScreenViewportChange?.Invoke(this, e); }
 
         public event EventHandler<PollInputEventArgs> PollInput;
         protected virtual void OnPollInput(PollInputEventArgs e) { PollInput?.Invoke(this, e); }

MasterFudgeMk2/Machines/Coleco/ColecoVision/Manager.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.IO;
+using System.Drawing;
 
 using MasterFudgeMk2.Common;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 using MasterFudgeMk2.Devices;
 
@@ -65,8 +64,11 @@ namespace MasterFudgeMk2.Machines.Coleco.ColecoVision
         public override string FriendlyName { get { return "Coleco ColecoVision"; } }
         public override string FriendlyShortName { get { return "ColecoVision"; } }
         public override string FileFilter { get { return "ColecoVision ROMs (*.col)|*.col"; } }
+
         public override double RefreshRate { get { return refreshRate; } }
         public override float AspectRatio { get { return (576.0f / 486.0f); } }
+        public override Rectangle ScreenViewport { get { return new Rectangle(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines); } }
+
         public override bool SupportsBootingWithoutMedia { get { return true; } }
         public override bool CanCurrentlyBootWithoutMedia { get { return File.Exists(configuration.BiosPath); } }
         public override MachineConfiguration Configuration { get { return configuration; } set { configuration = (value as Configuration); } }
@@ -175,8 +177,6 @@ namespace MasterFudgeMk2.Machines.Coleco.ColecoVision
             controlsReadMode = 0x00;
 
             isNmi = isNmiPending = false;
-
-            OnScreenViewportChange(new ScreenViewportChangeEventArgs(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines));
 
             base.Reset();
         }

MasterFudgeMk2/Machines/IMachineManager.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 
-using MasterFudgeMk2.Common.AudioBackend;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 
 namespace MasterFudgeMk2.Machines
@@ -21,6 +21,7 @@ namespace MasterFudgeMk2.Machines
         /// Get file filter for Open ROM dialog
         /// </summary>
         string FileFilter { get; }
+
         /// <summary>
         /// Get system refresh rate
         /// </summary>
@@ -29,6 +30,11 @@ namespace MasterFudgeMk2.Machines
         /// Get system aspect ratio
         /// </summary>
         float AspectRatio { get; }
+        /// <summary>
+        /// Get system screen viewport, ex. Game Gear's 160x144 viewport
+        /// </summary>
+        Rectangle ScreenViewport { get; }
+
         /// <summary>
         /// Get flag if system supports booting without media
         /// </summary>
@@ -51,10 +57,6 @@ namespace MasterFudgeMk2.Machines
         /// Event handler for rendering
         /// </summary>
         event EventHandler<RenderScreenEventArgs> RenderScreen;
-        /// <summary>
-        /// Event handler for viewport changes, ex. Game Gear's 160x144 viewport
-        /// </summary>
-        event EventHandler<ScreenViewportChangeEventArgs> ScreenViewportChange;
         /// <summary>
         /// Event handler for input polling
         /// </summary>

MasterFudgeMk2/Machines/Sega/GameGear/Manager.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 
 using MasterFudgeMk2.Common;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 using MasterFudgeMk2.Devices;
 using MasterFudgeMk2.Devices.Sega;
+using System.Drawing;
 
 namespace MasterFudgeMk2.Machines.Sega.GameGear
 {
@@ -40,8 +41,11 @@ namespace MasterFudgeMk2.Machines.Sega.GameGear
         public override string FriendlyName { get { return "Sega Game Gear"; } }
         public override string FriendlyShortName { get { return "Game Gear"; } }
         public override string FileFilter { get { return "Game Gear ROMs (*.gg)|*.gg"; } }
+
         public override double RefreshRate { get { return refreshRate; } }
         public override float AspectRatio { get { return (576.0f / 486.0f); } }
+        public override Rectangle ScreenViewport { get { return new Rectangle(SegaGGVDP.ScreenViewportX, ((vdp.NumScanlines / 2) - (SegaGGVDP.ScreenViewportHeight) / 2), SegaGGVDP.ScreenViewportWidth, SegaGGVDP.ScreenViewportHeight); } }
+
         public override bool SupportsBootingWithoutMedia { get { return false; } }
         public override bool CanCurrentlyBootWithoutMedia { get { return false; } }
         public override MachineConfiguration Configuration { get { return configuration; } set { configuration = (value as Configuration); } }
@@ -163,8 +167,6 @@ namespace MasterFudgeMk2.Machines.Sega.GameGear
             portRxBuffer = 0xFF;
             portSerialControl = 0x00;
             portStereoControl = 0xFF;
-
-            OnScreenViewportChange(new ScreenViewportChangeEventArgs(SegaGGVDP.ScreenViewportX, ((vdp.NumScanlines / 2) - (SegaGGVDP.ScreenViewportHeight) / 2), SegaGGVDP.ScreenViewportWidth, SegaGGVDP.ScreenViewportHeight));
 
             base.Reset();
         }

MasterFudgeMk2/Machines/Sega/MasterSystem/Manager.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.IO;
+using System.Drawing;
 
 using MasterFudgeMk2.Common;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 using MasterFudgeMk2.Devices;
 using MasterFudgeMk2.Devices.Sega;
@@ -54,8 +53,11 @@ namespace MasterFudgeMk2.Machines.Sega.MasterSystem
         public override string FriendlyName { get { return "Sega Master System"; } }
         public override string FriendlyShortName { get { return "Master System"; } }
         public override string FileFilter { get { return "Master System ROMs (*.sms)|*.sms"; } }
+
         public override double RefreshRate { get { return refreshRate; } }
         public override float AspectRatio { get { return (!configuration.IsPalSystem ? (576.0f / 486.0f) : (720.0f / 486.0f)); } }
+        public override Rectangle ScreenViewport { get { return new Rectangle(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines); } }
+
         public override bool SupportsBootingWithoutMedia { get { return true; } }
         public override bool CanCurrentlyBootWithoutMedia { get { return (File.Exists(configuration.BootstrapPath) && configuration.UseBootstrap); } }
         public override MachineConfiguration Configuration { get { return configuration; } set { configuration = (value as Configuration); } }
@@ -187,8 +189,6 @@ namespace MasterFudgeMk2.Machines.Sega.MasterSystem
             lastHCounter = 0x00;
 
             pauseButtonPressed = pauseButtonToggle = false;
-
-            OnScreenViewportChange(new ScreenViewportChangeEventArgs(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines));
 
             base.Reset();
         }

MasterFudgeMk2/Machines/Sega/SG1000/Manager.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Drawing;
 
 using MasterFudgeMk2.Common;
-using MasterFudgeMk2.Common.VideoBackend;
+using MasterFudgeMk2.Common.EventArguments;
 using MasterFudgeMk2.Media;
 using MasterFudgeMk2.Devices;
 
@@ -50,8 +49,11 @@ namespace MasterFudgeMk2.Machines.Sega.SG1000
         public override string FriendlyName { get { return "Sega SG-1000"; } }
         public override string FriendlyShortName { get { return "SG-1000"; } }
         public override string FileFilter { get { return "SG-1000 ROMs (*.sg)|*.sg"; } }
+
         public override double RefreshRate { get { return refreshRate; } }
         public override float AspectRatio { get { return (576.0f / 486.0f); } }
+        public override Rectangle ScreenViewport { get { return new Rectangle(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines); } }
+
         public override bool SupportsBootingWithoutMedia { get { return false; } }
         public override bool CanCurrentlyBootWithoutMedia { get { return false; } }
         public override MachineConfiguration Configuration { get { return configuration; } set { configuration = (value as Configuration); } }
@@ -149,9 +151,7 @@ namespace MasterFudgeMk2.Machines.Sega.SG1000
             portIoAB = portIoBMisc = 0xFF;
 
             pauseButtonPressed = pauseButtonToggle = false;
-
-            OnScreenViewportChange(new ScreenViewportChangeEventArgs(0, 0, TMS9918A.NumPixelsPerLine, vdp.NumScanlines));
-
+            
             base.Reset();
         }
 

commit 80bbd35df7bed2be04e21f607e9b8bbb029a3617

Screen viewport fix/kludge
GG screen viewport broke probably with commit 4b3e2dc, reintroduced some of the logic from before that; seems kinda messy, maybe rework later

MasterFudgeMk2/Common/EventArguments/ScreenViewportChangeEventArgs.cs
@@ -1,0 +1,20 @@
+﻿using System;
+
+namespace MasterFudgeMk2.Common.EventArguments
+{
+    public class ScreenViewportChangeEventArgs : EventArgs
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public ScreenViewportChangeEventArgs(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}

MasterFudgeMk2/Devices/Sega/SegaGGVDP.cs
@@ -30,6 +30,11 @@ namespace MasterFudgeMk2.Devices.Sega
             cramLatch = 0x0000;
         }
 
+        protected override void RearrangeFramebuffer()
+        {
+            //
+        }
+
         protected override int AdjustVCounter(int scanline)
         {
             int counter = scanline;

MasterFudgeMk2/Devices/TMS9918A.cs
@@ -234,7 +234,7 @@ namespace MasterFudgeMk2.Devices
             return drawScreen;
         }
 
-        protected void RearrangeFramebuffer()
+        protected virtual void RearrangeFramebuffer()
         {
             // TODO: a bit of a kludge, maybe rework somehow?
             int bytesUntilDisplayEnd = ((scanlineTopBlanking * NumActivePixelsPerScanline) * 4);

MasterFudgeMk2/Machines/BaseMachine.cs
@@ -30,6 +30,9 @@ namespace MasterFudgeMk2.Machines
         public event EventHandler<RenderScreenEventArgs> RenderScreen;
         protected virtual void OnRenderScreen(RenderScreenEventArgs e) { RenderScreen?.Invoke(this, e); }
 
+        public event EventHandler<ScreenViewportChangeEventArgs> ScreenViewportChange;
+        protected virtual void OnScreenViewportChange(ScreenViewportChangeEventArgs e) { ScreenViewportChange?.Invoke(this, e); }
+
         public event EventHandler<PollInputEventArgs> PollInput;
         protected virtual void OnPollInput(PollInputEventArgs e) { PollInput?.Invoke(this, e); }
 

MasterFudgeMk2/Machines/Coleco/ColecoVision/Manager.cs
@@ -233,7 +233,10 @@ namespace MasterFudgeMk2.Machines.Coleco.ColecoVision
             double currentMasterClockCycles = (currentCpuClockCycles * 3.0);
 
             if (vdp.Step((int)Math.Round(currentMasterClockCycles)))
+            {
+                OnScreenViewportChange(new ScreenViewportChangeEventArgs(ScreenViewport.X, ScreenViewport.Y, ScreenViewport.Width, ScreenViewport.Height));
                 OnRenderScreen(new RenderScreenEventArgs(TMS9918A.NumActivePixelsPerScanline, vdp.NumTotalScanlines, vdp.OutputFramebuffer));
+            }
 
             /* The IMO oddball NMI-Vblank handling, see ex. Cogwheel */
             if (vdp.InterruptLine == InterruptState.Assert && !isNmi) isNmiPending = true;

MasterFudgeMk2/Machines/IMachineManager.cs
@@ -68,6 +68,10 @@ namespace MasterFudgeMk2.Machines
         /// </summary>
         event EventHandler<RenderScreenEventArgs> RenderScreen;
         /// <summary>
+        /// Event handler for viewport changes, ex. Game Gear's 160x144 viewport
+        /// </summary>
+        event EventHandler<ScreenViewportChangeEventArgs> ScreenViewportChange;
+        /// <summary>
         /// Event handler for input polling
         /// </summary>
         event EventHandler<PollInputEventArgs> PollInput;

MasterFudgeMk2/Machines/Sega/GameGear/Manager.cs
@@ -46,8 +46,8 @@ namespace MasterFudgeMk2.Machines.Sega.GameGear
         public override string DatFileName { get { return "Sega - Game Gear.dat"; } }
 
         public override double RefreshRate { get { return refreshRate; } }
-        public override float AspectRatio { get { return (576.0f / 486.0f); } }
-        public override Rectangle ScreenViewport { get { return new Rectangle(SegaGGVDP.ScreenViewportX, ((vdp.NumTotalScanlines / 2) - (SegaGGVDP.ScreenViewportHeight) / 2), SegaGGVDP.ScreenViewportWidth, SegaGGVDP.ScreenViewportHeight); } }
+        public override float AspectRatio { get { return (SegaGGVDP.ScreenViewportWidth / (float)SegaGGVDP.ScreenViewportHeight); } }
+        public override Rectangle ScreenViewport { get { return new Rectangle(SegaGGVDP.ScreenViewportX, ((vdp.ScreenHeight / 2) - (SegaGGVDP.ScreenViewportHeight) / 2), SegaGGVDP.ScreenViewportWidth, SegaGGVDP.ScreenViewportHeight); } }
 
         public override bool SupportsBootingWithoutMedia { get { return false; } }
         public override bool CanCurrentlyBootWithoutMedia { get { return false; } }
@@ -212,7 +212,10 @@ namespace MasterFudgeMk2.Machines.Sega.GameGear
             double currentMasterClockCycles = (currentCpuClockCycles * 3.0);
 
             if (vdp.Step((int)Math.Round(currentMasterClockCycles)))
+            {
+                OnScreenViewportChange(new ScreenViewportChangeEventArgs(ScreenViewport.X, ScreenViewport.Y, ScreenViewport.Width, ScreenViewport.Height));
                 OnRenderScreen(new RenderScreenEventArgs(TMS9918A.NumActivePixelsPerScanline, vdp.NumTotalScanlines, vdp.OutputFramebuffer));
+            }
 
             cpu.SetInterruptLine(vdp.InterruptLine);
 

MasterFudgeMk2/Machines/Sega/MasterSystem/Manager.cs
@@ -244,7 +244,10 @@ namespace MasterFudgeMk2.Machines.Sega.MasterSystem
             double currentMasterClockCycles = (currentCpuClockCycles * 3.0);
 
             if (vdp.Step((int)Math.Round(currentMasterClockCycles)))
+            {
+                OnScreenViewportChange(new ScreenViewportChangeEventArgs(ScreenViewport.X, ScreenViewport.Y, ScreenViewport.Width, ScreenViewport.Height));
                 OnRenderScreen(new RenderScreenEventArgs(TMS9918A.NumActivePixelsPerScanline, vdp.NumTotalScanlines, vdp.OutputFramebuffer));
+            }
 
             if (pauseButtonPressed)
             {

MasterFudgeMk2/Machines/Sega/SG1000/Manager.cs
@@ -202,7 +202,10 @@ namespace MasterFudgeMk2.Machines.Sega.SG1000
             double currentMasterClockCycles = (currentCpuClockCycles * 3.0);
 
             if (vdp.Step((int)Math.Round(currentMasterClockCycles)))
+            {
+                OnScreenViewportChange(new ScreenViewportChangeEventArgs(ScreenViewport.X, ScreenViewport.Y, ScreenViewport.Width, ScreenViewport.Height));
                 OnRenderScreen(new RenderScreenEventArgs(TMS9918A.NumActivePixelsPerScanline, vdp.NumTotalScanlines, vdp.OutputFramebuffer));
+            }
 
             if (pauseButtonPressed)
             {

MasterFudgeMk2/MainForm.cs
@@ -314,8 +314,6 @@ namespace MasterFudgeMk2
                 ForceSquarePixels = emuConfig.ForceSquarePixels;
 
                 renderer.AspectRatio = machineManager.AspectRatio;
-
-                renderer.ScreenViewport = machineManager.ScreenViewport;
             }
 
             foreach (ToolStripMenuItem menuItem in videoBackendToolStripMenuItem.DropDownItems)
@@ -458,12 +456,12 @@ namespace MasterFudgeMk2
 
             machineManager = (Activator.CreateInstance(machineType) as IMachineManager);
             machineManager.RenderScreen += renderer.OnRenderScreen;
+            machineManager.ScreenViewportChange += renderer.OnScreenViewportChange;
             machineManager.PollInput += MachineManager_OnPollInput;
             machineManager.FrameEnded += MachineManager_FrameEnded;
             machineManager.AddSampleData += soundOutput.OnAddSampleData;
 
             renderer.AspectRatio = machineManager.AspectRatio;
-            renderer.ScreenViewport = machineManager.ScreenViewport;
 
             machineManager.Startup();
 

MasterFudgeMk2/MasterFudgeMk2.csproj
@@ -96,6 +96,7 @@
     </Compile>
     <Compile Include="Common\Crc32.cs" />
     <Compile Include="Common\Enumerations\InterruptState.cs" />
+    <Compile Include="Common\EventArguments\ScreenViewportChangeEventArgs.cs" />
     <Compile Include="Common\MustInitialize.cs" />
     <Compile Include="Common\TupleListExtensions.cs" />
     <Compile Include="Common\UiStateBoolean.cs" />

MasterFudgeMk2/NewMainForm.cs
@@ -370,9 +370,9 @@ namespace MasterFudgeMk2
             {
                 /* Configure new backend & attach to active machine */
                 activeVideoBackend.AspectRatio = activeMachine.AspectRatio;
-                activeVideoBackend.ScreenViewport = activeMachine.ScreenViewport;
-
+                
                 activeMachine.RenderScreen += activeVideoBackend.OnRenderScreen;
+                activeMachine.ScreenViewportChange += activeVideoBackend.OnScreenViewportChange;
             }
 
             /* Handle UI & emulation config updates */
@@ -495,10 +495,10 @@ namespace MasterFudgeMk2
             activeMachine.Startup();
 
             activeVideoBackend.AspectRatio = activeMachine.AspectRatio;
-            activeVideoBackend.ScreenViewport = activeMachine.ScreenViewport;
-
+            
             /* Video backend events */
             activeMachine.RenderScreen += activeVideoBackend.OnRenderScreen;
+            activeMachine.ScreenViewportChange += activeVideoBackend.OnScreenViewportChange;
 
             /* Audio backend events */
             activeMachine.AddSampleData += activeAudioBackend.OnAddSampleData;

MasterFudgeMk2/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.33.0")]
-[assembly: AssemblyFileVersion("0.1.33.0")]
+[assembly: AssemblyVersion("0.1.34.0")]
+[assembly: AssemblyFileVersion("0.1.34.0")]

MasterFudgeMk2/VideoBackends/BaseVideoBackend.cs
@@ -17,8 +17,7 @@ namespace MasterFudgeMk2.VideoBackends
         public virtual bool KeepAspectRatio { get { return keepAspectRatio; } set { keepAspectRatio = value; } }
         public virtual bool ForceSquarePixels { get { return forceSquarePixels; } set { forceSquarePixels = value; } }
         public virtual float AspectRatio { get { return aspectRatio; } set { aspectRatio = value; } }
-        public virtual Rectangle ScreenViewport { get { return screenViewport; } set { screenViewport = value; } }
-
+        
         public BaseVideoBackend(Control control)
         {
             outputControl = control;
@@ -30,5 +29,6 @@ namespace MasterFudgeMk2.VideoBackends
 
         public abstract void OnOutputResized(object sender, OutputResizedEventArgs e);
         public abstract void OnRenderScreen(object sender, RenderScreenEventArgs e);
+        public abstract void OnScreenViewportChange(object sender, ScreenViewportChangeEventArgs e);
     }
 }

MasterFudgeMk2/VideoBackends/Direct2DBackend.cs
@@ -43,7 +43,6 @@ namespace MasterFudgeMk2.VideoBackends
         public override bool KeepAspectRatio { get { return keepAspectRatio; } set { keepAspectRatio = value; ResizeRenderTargetAndDestinationRectangle(); } }
         public override bool ForceSquarePixels { get { return forceSquarePixels; } set { forceSquarePixels = value; ResizeRenderTargetAndDestinationRectangle(); } }
         public override float AspectRatio { get { return aspectRatio; } set { aspectRatio = value; ResizeRenderTargetAndDestinationRectangle(); } }
-        public override DrawingRectangle ScreenViewport { get { return screenViewport; } set { screenViewport = value; ResizeRenderTargetAndDestinationRectangle(); } }
 
         public Direct2DBackend(Control control) : base(control)
         {
@@ -115,7 +114,7 @@ namespace MasterFudgeMk2.VideoBackends
             DrawingBitmapData bmpData = fullScreenshot.LockBits(new DrawingRectangle(0, 0, fullScreenshot.Width, fullScreenshot.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, fullScreenshot.PixelFormat);
 
             byte[] pixelData = new byte[bmpData.Stride * bmpData.Height];
-            System.Buffer.BlockCopy(lastFrameData, 0, pixelData, 0, pixelData.Length);
+            System.Buffer.BlockCopy(lastFrameData, 0, pixelData, 0, lastFrameData.Length);
             System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, pixelData.Length);
 
             fullScreenshot.UnlockBits(bmpData);
@@ -182,6 +181,12 @@ namespace MasterFudgeMk2.VideoBackends
             renderTarget.Clear(Color.Black);
             renderTarget.DrawBitmap(bitmapRenderTarget.Bitmap, destinationRectangle, 1.0f, BitmapInterpolationMode.Linear, sourceRectangle);
             renderTarget.EndDraw();
+        }
+
+        public override void OnScreenViewportChange(object sender, ScreenViewportChangeEventArgs e)
+        {
+            screenViewport = new DrawingRectangle(e.X, e.Y, e.Width, e.Height);
+            ResizeRenderTargetAndDestinationRectangle();
         }
     }
 }

MasterFudgeMk2/VideoBackends/IVideoBackend.cs
@@ -10,11 +10,11 @@ namespace MasterFudgeMk2.VideoBackends
         bool KeepAspectRatio { get; set; }
         bool ForceSquarePixels { get; set; }
         float AspectRatio { get; set; }
-        Rectangle ScreenViewport { get; set; }
 
         Bitmap GetRawScreenshot();
 
         void OnOutputResized(object sender, OutputResizedEventArgs e);
         void OnRenderScreen(object sender, RenderScreenEventArgs e);
+        void OnScreenViewportChange(object sender, ScreenViewportChangeEventArgs e);
     }
 }

MasterFudgeMk2/VideoBackends/NullVideoBackend.cs
@@ -26,5 +26,6 @@ namespace MasterFudgeMk2.VideoBackends
                 g.Clear(Color.Black);
             }
         }
+        public override void OnScreenViewportChange(object sender, ScreenViewportChangeEventArgs e) { }
     }
 }