bc113ce25751340806b57789128abab1b3d7bb75
xdaniel <xdanieldzd@users.noreply.github.com>
2018-02-09 16:10:38 +0100

    SNES: Added skeleton from wip-snes branch
    Added skeleton code from wip-snes branch; old branch will be deleted

---
+++ MasterFudgeMk2/Machines/Nintendo/SuperNintendo/Configuration.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace MasterFudgeMk2.Machines.Nintendo.SuperNintendo
+{
+    public sealed class Configuration : MachineConfiguration
+    {
+        public override sealed string Name { get { return "SuperNintendo.xml"; } }
+
+        /* Settings */
+        [Description("Emulate PAL System")]
+        [XmlElement]
+        public bool IsPalSystem { get; set; } = false;
+
+        /* Inputs (General) */
+        [XmlIgnore]
+        public Enum Reset { get; set; } = null;
+
+        [XmlElement(ElementName = nameof(Reset)), ReadOnly(true)]
+        public string ResetString { get { return Reset?.GetFullyQualifiedName(); } set { Reset = value?.GetEnumFromFullyQualifiedName(); } }
+
+        /* Inputs (P1) */
+        [XmlIgnore]
+        public Enum P1Up { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Down { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Left { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Right { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Start { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Select { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1L { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1R { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1B { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1A { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1Y { get; set; } = null;
+        [XmlIgnore]
+        public Enum P1X { get; set; } = null;
+
+        [XmlElement(ElementName = nameof(P1Up)), ReadOnly(true)]
+        public string P1UpString { get { return P1Up?.GetFullyQualifiedName(); } set { P1Up = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Down)), ReadOnly(true)]
+        public string P1DownString { get { return P1Down?.GetFullyQualifiedName(); } set { P1Down = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Left)), ReadOnly(true)]
+        public string P1LeftString { get { return P1Left?.GetFullyQualifiedName(); } set { P1Left = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Right)), ReadOnly(true)]
+        public string P1RightString { get { return P1Right?.GetFullyQualifiedName(); } set { P1Right = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Start)), ReadOnly(true)]
+        public string P1StartString { get { return P1Start?.GetFullyQualifiedName(); } set { P1Start = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Select)), ReadOnly(true)]
+        public string P1SelectString { get { return P1Select?.GetFullyQualifiedName(); } set { P1Select = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1L)), ReadOnly(true)]
+        public string P1LString { get { return P1L?.GetFullyQualifiedName(); } set { P1L = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1R)), ReadOnly(true)]
+        public string P1RString { get { return P1R?.GetFullyQualifiedName(); } set { P1R = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1B)), ReadOnly(true)]
+        public string P1BString { get { return P1B?.GetFullyQualifiedName(); } set { P1B = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1A)), ReadOnly(true)]
+        public string P1AString { get { return P1A?.GetFullyQualifiedName(); } set { P1A = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1Y)), ReadOnly(true)]
+        public string P1YString { get { return P1Y?.GetFullyQualifiedName(); } set { P1Y = value?.GetEnumFromFullyQualifiedName(); } }
+        [XmlElement(ElementName = nameof(P1X)), ReadOnly(true)]
+        public string P1XString { get { return P1X?.GetFullyQualifiedName(); } set { P1X = value?.GetEnumFromFullyQualifiedName(); } }
+
+        public Configuration() : base() { }
+    }
+}
---
+++ MasterFudgeMk2/Machines/Nintendo/SuperNintendo/Manager.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+
+using MasterFudgeMk2.Common;
+using MasterFudgeMk2.Common.Enumerations;
+using MasterFudgeMk2.Common.EventArguments;
+using MasterFudgeMk2.Media;
+using MasterFudgeMk2.Devices;
+
+namespace MasterFudgeMk2.Machines.Nintendo.SuperNintendo
+{
+    [TypeConverter(typeof(DescriptionTypeConverter))]
+    public enum MachineInputs
+    {
+        [Description("Player 1: Up")]
+        P1Up,
+        [Description("Player 1: Down")]
+        P1Down,
+        [Description("Player 1: Left")]
+        P1Left,
+        [Description("Player 1: Right")]
+        P1Right,
+        [Description("Player 1: L")]
+        P1L,
+        [Description("Player 1: R")]
+        P1R,
+        [Description("Player 1: B")]
+        P1B,
+        [Description("Player 1: A")]
+        P1A,
+        [Description("Player 1: Y")]
+        P1Y,
+        [Description("Player 1: X")]
+        P1X,
+
+        [Description("Reset Button")]
+        Reset
+    }
+
+    class Manager : BaseMachine
+    {
+        public override string FriendlyName { get { return "Super Nintendo"; } }
+        public override string FriendlyShortName { get { return "SNES"; } }
+        public override string FileFilter { get { return "Super Nintendo ROMs (*.smc)|*.smc"; } }
+        public override string DatFileName { get { return "Nintendo - Super Nintendo Entertainment System.dat"; } }
+
+        public override double RefreshRate { get { return refreshRate[clockIndex]; } }
+        public override float AspectRatio { get { return (!configuration.IsPalSystem ? (576.0f / 486.0f) : (720.0f / 486.0f)); } }
+        public override Rectangle ScreenViewport
+        {
+            get
+            {
+                // viewport
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool SupportsBootingWithoutMedia { get { return false; } }
+        public override bool CanCurrentlyBootWithoutMedia { get { return false; } }
+        public override string[] MediaSlots { get { return new string[] { "Cartridge Slot" }; } }
+
+        public override MachineConfiguration Configuration { get { return configuration; } set { configuration = (value as Configuration); } }
+
+        public override List<Tuple<string, Type, double>> DebugChipInformation
+        {
+            get
+            {
+                return new List<Tuple<string, Type, double>>
+                {
+                    //chips
+                };
+            }
+        }
+
+        /* Constants */
+        static readonly double[] crystal = { 21477270.0, 17734475.0 };
+        static readonly double[] colorClock = { (crystal[0] / 6.0), (crystal[1] / 4.0) };
+        static readonly double[] masterClock = { (crystal[0] / 1.0), (crystal[1] * 6.0 / 5.0) };
+        static readonly double[] dotClock = { (crystal[0] / 4.0), (masterClock[1] / 4.0) };
+        static readonly double[] cpuClockNoWait = { (crystal[0] / 6.0), (masterClock[1] / 6.0) };
+        static readonly double[] cpuClockShortWait = { (crystal[0] / 8.0), (masterClock[1] / 8.0) };
+        static readonly double[] cpuClockJoyWait = { (crystal[0] / 8.0), (masterClock[1] / 8.0) };
+        static readonly double[] refreshRate = { (crystal[0] / (262.0 * 1364.0 - 4.0 / 2.0)), (masterClock[1] / (312.0 * 1364.0)) };
+        static readonly double[] interlace = { (crystal[0] / (525.0 * 1364.0)), (masterClock[1] / (625.0 * 1364.0 + 4.0 / 2.0)) };
+
+        const int ramSize = 1 * 8192;
+
+        /* Devices on bus */
+        // cpu, ppu, etc
+
+        // ioport flags or w/e
+
+        // ports
+
+        int clockIndex { get { return (configuration.IsPalSystem ? 1 : 0); } }
+
+        protected override int totalMasterClockCyclesInFrame { get { return (int)Math.Round(masterClock[clockIndex] / refreshRate[clockIndex]); } }
+
+        // port enable/disable checks, if applicable...?
+
+        Configuration configuration;
+
+        public Manager()
+        {
+            configuration = ConfigFile.Load<Configuration>();
+
+            // null cart etc
+
+            // create devices
+        }
+
+        public override void Startup()
+        {
+            // startup devices
+
+            Reset();
+        }
+
+        public override void Reset()
+        {
+            // reset devices
+
+            base.Reset();
+        }
+
+        public override bool CanLoadMedia(FileInfo mediaFile)
+        {
+            // check file
+
+            return true;
+        }
+
+        public override void LoadMedia(int slotNumber, IMedia media)
+        {
+            // load media
+        }
+
+        public override void SaveMedia()
+        {
+            //
+        }
+
+        public override void Shutdown()
+        {
+            // unload/shutdown
+        }
+
+        public override void RunStep()
+        {
+            double currentCpuClockCycles = 0.0;
+            // step cpu
+            double currentMasterClockCycles = 0.0;
+
+            // step other devices
+            // check reset, interrupts, etc
+
+            currentMasterClockCyclesInFrame += (int)Math.Round(currentMasterClockCycles);
+        }
+
+        protected override void ParseInput(PollInputEventArgs input)
+        {
+            // do input
+        }
+
+        private byte ReadMemory(ushort address)
+        {
+            // read memory
+            return 0x00;
+        }
+
+        private void WriteMemory(ushort address, byte value)
+        {
+            // write memory
+        }
+
+        private byte ReadPort(byte port)
+        {
+            // read ports
+            return 0x00;
+        }
+
+        public void WritePort(byte port, byte value)
+        {
+            // write ports
+        }
+    }
+}
